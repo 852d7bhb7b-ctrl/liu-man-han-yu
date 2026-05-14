0e78aee2e5a942e8e2c0c763cb7f742051b04cac
Gary <852d7bhb7b@privaterelay.appleid.com>
2026-05-14 14:02:14 +0800

    fix: 恢复 plugins 方式 + 移除代理

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 // Top-level build file
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.5.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-    }
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
