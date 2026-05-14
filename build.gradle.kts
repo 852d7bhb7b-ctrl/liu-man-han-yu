6e07c01fc9379d0c5e5d5440001e3b70996bd3b5
Gary <852d7bhb7b@privaterelay.appleid.com>
2026-05-14 12:00:37 +0800

    fix: 改用 buildscript + apply plugin 方式

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,3 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
-
-plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,6 @@
-pluginManagement {
-    repositories {
-        maven("https://dl.google.com/dl/android/maven2/")
-        maven("https://maven.google.com")
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://dl.google.com/dl/android/maven2/")
         google()
         mavenCentral()
     }
